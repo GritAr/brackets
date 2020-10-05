197f30c24c6a8956fff826fd65a86a7196b69241
Artem <thistlefurcat@gmail.com>
2020-10-05 20:30:57 +0300

    feat: add solution to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let pair = [],
+      modStr = str;
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    for (let j = 0; j < bracketsConfig[i].length; j++) {
+      pair.push(bracketsConfig[i][0] + bracketsConfig[i][1]);
+    }
+  }
+  pair = [...new Set(pair)];
+  
+    for (let i = 0; i < str.length; i++) {
+      for(let j = 0; j < pair.length; j++) {
+        modStr = modStr.replace(pair[j], '');
+        if (modStr.length === 0) return true;
+      }
+    } 
+    return false;
+  }
